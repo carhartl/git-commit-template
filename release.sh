98cad1db7482c80c5c90a1b80f476e907e67388a
Klaus Hartl <klaus.hartl@gmail.com>
2023-06-05 12:25:57 +0200

    Fix version string produced by release script

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 set -eu
 
 versionregex='v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'
-grep -rl -E "$versionregex" --exclude-dir=.git --exclude-dir=.github --exclude-dir=.vscode --exclude-dir=dist --exclude=go.mod --exclude=go.sum . | xargs sed -i '' -E 's/'"$versionregex"'/'"$1"'/g'
+grep -rl -E "$versionregex" --exclude-dir=.git --exclude-dir=.github --exclude-dir=.vscode --exclude-dir=dist --exclude=go.mod --exclude=go.sum . | xargs sed -i '' -E 's/'"$versionregex"'/'"v$1"'/g'
 git commit -m "Update for v$1"
 git tag "v$1" -m "Release v$1"
 goreleaser release --clean
