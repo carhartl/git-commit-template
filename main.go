629b57a1d966f57fd384462e57f79ef1864cf9b2
Klaus Hartl <klaus.hartl@gmail.com>
2023-06-09 14:16:23 +0200

    Update for v0.2.0

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := &cli.App{
 		Name:    "git-commit-template",
 		Usage:   "Set up a useful template for commit messages",
-		Version: "v0.1.1",
+		Version: "v0.2.0",
 		Commands: []*cli.Command{
 			command.SetTemplateCommand,
 			command.UnsetTemplateCommand,
