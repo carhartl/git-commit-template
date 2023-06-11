cf79dad7a5298625500033675c0cf367a93cac75
Klaus Hartl <klaus.hartl@gmail.com>
2023-06-11 07:51:08 +0200

    Improve stashing away changes before releasing
    Goreleaser would notice untracked files and abort...
    
    https://github.com/goreleaser/goreleaser/blob/25c3ed2a7f4b091db8fbbdf5e68a797ab524352b/internal/pipe/git/git.go#L196

--- release.sh
+++ release.sh
@@ -4,11 +4,11 @@ set -eu
 
 versionregex='v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'
 
-git stash
+git stash --include-untracked
 grep -rl -E "$versionregex" --exclude-dir=.git --exclude-dir=.github --exclude-dir=.vscode --exclude-dir=dist --exclude=go.mod --exclude=go.sum . | xargs sed -i '' -E 's/'"$versionregex"'/'"v$1"'/g'
 git add .
 git commit -m "Update for v$1"
 git tag "v$1" -m "Release v$1"
 git push
 goreleaser release --clean
-git stash pop
+git stash pop --index
